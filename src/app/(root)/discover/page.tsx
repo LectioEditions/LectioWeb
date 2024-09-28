45b474f9a6833b78cd671ea55a9be6bd47f9f4a7
yassinkr <kerraiyassin@gmail.com>
2024-09-28 18:37:59 +0100

    fixing discover page

--- src/app/(root)/discover/page.tsx
+++ src/app/(root)/discover/page.tsx
@@ -1,13 +1,13 @@
 import EmptyState from '@/src/components/EmptyState';
 import CoursCard from '@/src/components/CoursCard';
 import SearchBar from '@/src/components/SearchBar';
-import { getCoursBySearch, getCourses } from '@/src/server/db';
+import { getItemBySearch, getItemes } from '@/src/server/db';
 import { Loader } from 'lucide-react';
 import React from 'react'
 
 const Discover = async ({searchParams: {search}}:{searchParams:{search : string}}) => {
-  const courss= await getCoursBySearch(search ? search : '');
-  const discover = await getCourses();
+  const courss= await getItemBySearch(search ? search : '');
+  const discover = await getItemes();
   return (
     <div className=" flex flex-col gap-9">
       <SearchBar/>
