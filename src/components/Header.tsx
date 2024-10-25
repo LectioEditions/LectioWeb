bea9ebd6818b5e0b79f50dbcd69da87a75bd90a2
yassinkr <kerraiyassin@gmail.com>
2024-10-25 21:48:49 +0100

    final final6

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Header = ({headerTitle, titleClassName}:{headerTitle :string , titleClassName :string}) => {
   return (
-    <header className=' flex items-center justify-between'>
+    <header className=' flex items-center justify-between my-5'>
     {headerTitle ? (
         <h1 className={cn('text-lg font-bold text-black-1 dark:text-white-1', titleClassName)}
         >{headerTitle}</h1>
