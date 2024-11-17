e5d8bed995fab8d62aacf9284ebc032da836242b
LectioEditions <LectioEditions@outlook.com>
2024-11-17 20:20:50 +0100

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -28,6 +28,7 @@ export const Types = [
 
 ];
 export const Modules = [
+  // Première année
   { module: "Organique", annee: "1ere Année", departement: "Pharmacie" },
   { module: "Physique", annee: "1ere Année", departement: "Pharmacie" },
   { module: "Anatomie", annee: "1ere Année", departement: "Pharmacie" },
@@ -40,6 +41,8 @@ export const Modules = [
   { module: "Chimie organique", annee: "1ere Année", departement: "Pharmacie" },
   { module: "Physiologie", annee: "1ere Année", departement: "Pharmacie" },
   { module: "Langue française", annee: "1ere Année", departement: "Pharmacie" },
+
+  // Deuxième année
   { module: "Chimie analytique", annee: "2eme Année", departement: "Pharmacie" },
   { module: "Chimie Minérale", annee: "2eme Année", departement: "Pharmacie" },
   { module: "Biochimie", annee: "2eme Année", departement: "Pharmacie" },
@@ -48,25 +51,33 @@ export const Modules = [
   { module: "Génétique", annee: "2eme Année", departement: "Pharmacie" },
   { module: "Physiopathologie", annee: "2eme Année", departement: "Pharmacie" },
   { module: "Langue anglaise", annee: "2eme Année", departement: "Pharmacie" },
+
+  // Troisième année
   { module: "Pharmacologie", annee: "3eme Année", departement: "Pharmacie" },
   { module: "Chimie thérapeutique", annee: "3eme Année", departement: "Pharmacie" },
   { module: "Chimie analytique", annee: "3eme Année", departement: "Pharmacie" },
   { module: "Pharmacie galénique", annee: "3eme Année", departement: "Pharmacie" },
   { module: "Pharmacognosie", annee: "3eme Année", departement: "Pharmacie" },
   { module: "Sémiologie", annee: "3eme Année", departement: "Pharmacie" },
+
+  // Quatrième année
   { module: "Microbiologie", annee: "4eme Année", departement: "Pharmacie" },
   { module: "Biochimie", annee: "4eme Année", departement: "Pharmacie" },
   { module: "Immunologie", annee: "4eme Année", departement: "Pharmacie" },
   { module: "Parasitologie", annee: "4eme Année", departement: "Pharmacie" },
   { module: "Hemobiologie", annee: "4eme Année", departement: "Pharmacie" },
+
+  // Cinquième année
   { module: "Toxicologie", annee: "5eme Année", departement: "Pharmacie" },
   { module: "Hydrologie bromatologie", annee: "5eme Année", departement: "Pharmacie" },
   { module: "Pharmacie clinique", annee: "5eme Année", departement: "Pharmacie" },
   { module: "Épidémiologie", annee: "5eme Année", departement: "Pharmacie" },
   { module: "Gestion pharmaceutique", annee: "5eme Année", departement: "Pharmacie" },
   { module: "Droit pharmaceutique", annee: "5eme Année", departement: "Pharmacie" },
-  { module: "Pharmacie hospitaliere", annee: "5eme Année", departement: "Pharmacie" },
+  { module: "Pharmacie hospitalière", annee: "5eme Année", departement: "Pharmacie" },
   { module: "Pharmacie industrielle", annee: "5eme Année", departement: "Pharmacie" },
+  
+    // Première année
 
   { module: "Anatomie humaine", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Biochimie", annee: "1ere Année", departement: "Chirurgie Dentaire" },
@@ -75,44 +86,161 @@ export const Modules = [
   { module: "Physique", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Génétique", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Cytologie", annee: "1ere Année", departement: "Chirurgie Dentaire" },
-  { module: "Déontologie", annee: "1ere Année", departement: "Chirurgie Dentaire" },
+  { module: "Déontologie (SSH)", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Physiologie générale", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Biomathématiques", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Histologie", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Embryologie", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Français", annee: "1ere Année", departement: "Chirurgie Dentaire" },
   { module: "Anglais", annee: "1ere Année", departement: "Chirurgie Dentaire" },
+
+  // Deuxième année
   { module: "Anatomie humaine", annee: "2eme Année", departement: "Chirurgie Dentaire" },
   { module: "Anatomie dentaire", annee: "2eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Occlusion", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+  { module: "OCE", annee: "2eme Année", departement: "Chirurgie Dentaire" },
   { module: "Parodontologie", annee: "2eme Année", departement: "Chirurgie Dentaire" },
   { module: "Pathologie bucco-dentaire", annee: "2eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Orthopédie dento-faciale", annee: "2eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Prothese dentaire", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+  { module: "ODF", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Prothèse", annee: "2eme Année", departement: "Chirurgie Dentaire" },
   { module: "Microbiologie", annee: "2eme Année", departement: "Chirurgie Dentaire" },
   { module: "Immunologie", annee: "2eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Hygiene", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Hygiène", annee: "2eme Année", departement: "Chirurgie Dentaire" },
   { module: "Informatique", annee: "2eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Biomatériaux", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Biomatériaux (OCE et Prothèse)", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Histologie", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Anglais", annee: "2eme Année", departement: "Chirurgie Dentaire" },
   { module: "Physiologie spéciale", annee: "2eme Année", departement: "Chirurgie Dentaire" },
+
+  // Troisième année
+  { module: "OCE", annee: "3eme Année", departement: "Chirurgie Dentaire" },
+  { module: "ODF", annee: "3eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Parodontologie", annee: "3eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Prothèse", annee: "3eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Pathologie bucco-dentaire", annee: "3eme Année", departement: "Chirurgie Dentaire" },
   { module: "Pharmacologie", annee: "3eme Année", departement: "Chirurgie Dentaire" },
   { module: "Anesthésiologie", annee: "3eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Occlusion", annee: "3eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Orthopédie dento-faciale", annee: "3eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Parodontologie", annee: "3eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Prothese dentaire", annee: "3eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Pathologie bucco-dentaire", annee: "3eme Année", departement: "Chirurgie Dentaire" },
   { module: "Occlusodontie", annee: "3eme Année", departement: "Chirurgie Dentaire" },
   { module: "Anatomie pathologique", annee: "3eme Année", departement: "Chirurgie Dentaire" },
   { module: "Oxyologie", annee: "3eme Année", departement: "Chirurgie Dentaire" },
   { module: "Radiologie", annee: "3eme Année", departement: "Chirurgie Dentaire" },
+
+  // Quatrième année
+  { module: "OCE", annee: "4eme Année", departement: "Chirurgie Dentaire" },
+  { module: "ODF", annee: "4eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Parodontologie", annee: "4eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Pathologie bucco-dentaire", annee: "4eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Prothèse", annee: "4eme Année", departement: "Chirurgie Dentaire" },
   { module: "Implantologie", annee: "4eme Année", departement: "Chirurgie Dentaire" },
   { module: "Odontologie pédiatrique", annee: "4eme Année", departement: "Chirurgie Dentaire" },
   { module: "Odontologie gériatrique", annee: "4eme Année", departement: "Chirurgie Dentaire" },
   { module: "Déontologie", annee: "4eme Année", departement: "Chirurgie Dentaire" },
   { module: "Pathologie médicale", annee: "4eme Année", departement: "Chirurgie Dentaire" },
+
+  // Cinquième année
+  { module: "OCE", annee: "5eme Année", departement: "Chirurgie Dentaire" },
+  { module: "ODF", annee: "5eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Pathologie bucco-dentaire", annee: "5eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Prothèse", annee: "5eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Parodontologie", annee: "5eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Implantologie", annee: "5eme Année", departement: "Chirurgie Dentaire" },
+  { module: "Odontologie pédiatrique", annee: "5eme Année", departement: "Chirurgie Dentaire" },
   { module: "Ergonomie", annee: "5eme Année", departement: "Chirurgie Dentaire" },
-  { module: "Orthopédie et Prothese dentaires", annee: "5eme Année", departement: "Chirurgie Dentaire" }
+  { module: "OPS", annee: "5eme Année", departement: "Chirurgie Dentaire" },
+ 
+  // Première année
+  { module: "Biochimie", unite: "Unité 01", annee: "1ère Année", departement: "Médecine" },
+  { module: "Anatomie humaine", unite: "Unité 02", annee: "1ère Année", departement: "Médecine" },
+  { module: "Histologie", unite: "Unité 03", annee: "1ère Année", departement: "Médecine" },
+  { module: "Physiologie", unite: "Unité 04", annee: "1ère Année", departement: "Médecine" },
+  { module: "Embryologie", unite: "Unité 05", annee: "1ère Année", departement: "Médecine" },
+
+  // Deuxième année
+  { module: "Anatomie", unite: "Unité 01", annee: "2ème Année", departement: "Médecine" },
+  { module: "Physiologie", unite: "Unité 01", annee: "2ème Année", departement: "Médecine" },
+  { module: "Histologie", unite: "Unité 01", annee: "2ème Année", departement: "Médecine" },
+  { module: "Biophysique", unite: "Unité 01", annee: "2ème Année", departement: "Médecine" },
+
+  { module: "Anatomie", unite: "Unité 02", annee: "2ème Année", departement: "Médecine" },
+  { module: "Physiologie", unite: "Unité 02", annee: "2ème Année", departement: "Médecine" },
+  { module: "Histologie", unite: "Unité 02", annee: "2ème Année", departement: "Médecine" },
+  { module: "Biochimie", unite: "Unité 02", annee: "2ème Année", departement: "Médecine" },
+
+  { module: "Anatomie", unite: "Unité 03", annee: "2ème Année", departement: "Médecine" },
+  { module: "Physiologie", unite: "Unité 03", annee: "2ème Année", departement: "Médecine" },
+  { module: "Histologie", unite: "Unité 03", annee: "2ème Année", departement: "Médecine" },
+  { module: "Biochimie", unite: "Unité 03", annee: "2ème Année", departement: "Médecine" },
+
+  { module: "Anatomie", unite: "Unité 04", annee: "2ème Année", departement: "Médecine" },
+  { module: "Physiologie", unite: "Unité 04", annee: "2ème Année", departement: "Médecine" },
+  { module: "Histologie", unite: "Unité 04", annee: "2ème Année", departement: "Médecine" },
+  { module: "Biochimie", unite: "Unité 04", annee: "2ème Année", departement: "Médecine" },
+
+  { module: "Anatomie", unite: "Unité 05", annee: "2ème Année", departement: "Médecine" },
+  { module: "Physiologie", unite: "Unité 05", annee: "2ème Année", departement: "Médecine" },
+  { module: "Histologie", unite: "Unité 05", annee: "2ème Année", departement: "Médecine" },
+  { module: "Biophysique", unite: "Unité 05", annee: "2ème Année", departement: "Médecine" },
+
+  { module: "Génétique", unite: "Unité 06", annee: "2ème Année", departement: "Médecine" },
+  { module: "Immunologie", unite: "Unité 07", annee: "2ème Année", departement: "Médecine" },
+
+  // Troisième année
+  { module: "Sémiologie", unite: "Unité 01", annee: "3ème Année", departement: "Médecine" },
+  { module: "Radiologie", unite: "Unité 01", annee: "3ème Année", departement: "Médecine" },
+  { module: "Biochimie", unite: "Unité 01", annee: "3ème Année", departement: "Médecine" },
+  { module: "Physio-pathologie", unite: "Unité 01", annee: "3ème Année", departement: "Médecine" },
+  { module: "Physiologie Médicale", unite: "Unité 01", annee: "3ème Année", departement: "Médecine" },
+
+  { module: "Sémiologie", unite: "Unité 02", annee: "3ème Année", departement: "Médecine" },
+  { module: "Radiologie", unite: "Unité 02", annee: "3ème Année", departement: "Médecine" },
+  { module: "Biochimie", unite: "Unité 02", annee: "3ème Année", departement: "Médecine" },
+  { module: "Physio-pathologie", unite: "Unité 02", annee: "3ème Année", departement: "Médecine" },
+
+  { module: "Sémiologie", unite: "Unité 03", annee: "3ème Année", departement: "Médecine" },
+  { module: "Radiologie", unite: "Unité 03", annee: "3ème Année", departement: "Médecine" },
+  { module: "Biochimie", unite: "Unité 03", annee: "3ème Année", departement: "Médecine" },
+  { module: "Physio-pathologie", unite: "Unité 03", annee: "3ème Année", departement: "Médecine" },
+
+  { module: "Sémiologie", unite: "Unité 04", annee: "3ème Année", departement: "Médecine" },
+  { module: "Radiologie", unite: "Unité 04", annee: "3ème Année", departement: "Médecine" },
+  { module: "Biochimie", unite: "Unité 04", annee: "3ème Année", departement: "Médecine" },
+  { module: "Physio-pathologie", unite: "Unité 04", annee: "3ème Année", departement: "Médecine" },
+
+  { module: "Parasitologie", unite: "Unité 05", annee: "3ème Année", departement: "Médecine" },
+  { module: "Pharmacologie Clinique", unite: "Unité 06", annee: "3ème Année", departement: "Médecine" },
+  { module: "Microbiologie", unite: "Unité 07", annee: "3ème Année", departement: "Médecine" },
+  { module: "Immunologie", unite: "Unité 08", annee: "3ème Année", departement: "Médecine" },
+  { module: "Anatomopathologie", unite: "Unité 09", annee: "3ème Année", departement: "Médecine" },
+
+  // Quatrième année
+  { module: "Cardiologie", unite: "Unité 01", annee: "4ème Année", departement: "Médecine" },
+  { module: "Neurologie", unite: "Unité 02", annee: "4ème Année", departement: "Médecine" },
+  { module: "Infectiologie", unite: "Unité 03", annee: "4ème Année", departement: "Médecine" },
+  { module: "Gastro-Entérologie", unite: "Unité 04", annee: "4ème Année", departement: "Médecine" },
+  { module: "Onco-hématologie", unite: "Unité 05", annee: "4ème Année", departement: "Médecine" },
+  { module: "Pneumologie", unite: "Unité 06", annee: "4ème Année", departement: "Médecine" },
+
+  // Cinquième année
+  { module: "Gynécologie-Obstétrique", unite: "Unité 01", annee: "5ème Année", departement: "Médecine" },
+  { module: "Néphrologie et Urologie", unite: "Unité 02", annee: "5ème Année", departement: "Médecine" },
+  { module: "Endocrinologie", unite: "Unité 03", annee: "5ème Année", departement: "Médecine" },
+  { module: "Pédiatrie", unite: "Unité 04", annee: "5ème Année", departement: "Médecine" },
+  { module: "Appareil Locomoteur", unite: "Unité 05", annee: "5ème Année", departement: "Médecine" },
+  { module: "Psychiatrie", unite: "Unité 06", annee: "5ème Année", departement: "Médecine" },
+
+  // Sixième année
+  { module: "Ophtalmologie", unite: "Unité 01", annee: "6ème Année", departement: "Médecine" },
+  { module: "Urgence médico-chirurgicale", unite: "Unité 02", annee: "6ème Année", departement: "Médecine" },
+  { module: "Epidémiologie", unite: "Unité 03", annee: "6ème Année", departement: "Médecine" },
+  { module: "Oto-rhino-laryngologie", unite: "Unité 04", annee: "6ème Année", departement: "Médecine" },
+  { module: "Médecine du travail", unite: "Unité 05", annee: "6ème Année", departement: "Médecine" },
+  { module: "Médecine Légale", unite: "Unité 06", annee: "6ème Année", departement: "Médecine" },
+  { module: "Dermatologie", unite: "Unité 07", annee: "6ème Année", departement: "Médecine" }
+
+  
+
+
+
+  
 ];
 
 
