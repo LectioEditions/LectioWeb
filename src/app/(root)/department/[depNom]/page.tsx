ca5de676c1167e8a365e8bd7acbb57c8148db7d1
LectioEditions <LectioEditions@outlook.com>
2024-12-10 10:19:35 +0100

    Update page.tsx

--- src/app/(root)/department/[depNom]/page.tsx
+++ src/app/(root)/department/[depNom]/page.tsx
@@ -3,8 +3,11 @@ import { getItemByCategory } from '@/src/server/db';
 import React from 'react'
 
 const Page = async ({params}:{ params:{depNom : string}}) => {
-  const search = await params.depNom;
+  const DepNom = await params.depNom;
+  const capitalizeWords = (str: string) => 
+    str.replace(/\b\w/g, (char) => char.toUpperCase());
 
+  const search = capitalizeWords(DepNom);
   const Items = await getItemByCategory(search);
   return (
     <section>
