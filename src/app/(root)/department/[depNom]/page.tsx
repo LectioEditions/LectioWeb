d48b6c093c997d02695b4f1af7f3cb370270c261
LectioEditions <LectioEditions@outlook.com>
2024-12-28 12:49:33 +0100

    Update page.tsx

--- src/app/(root)/department/[depNom]/page.tsx
+++ src/app/(root)/department/[depNom]/page.tsx
@@ -2,10 +2,20 @@ import DepItems from '@/src/components/DepItems';
 import { getItemByCategory } from '@/src/server/db';
 import React from 'react'
 import { Categories } from '@/src/constants';
+import { auth } from '@clerk/nextjs/server';
+import { Loader } from 'lucide-react';
+
+
 const Page = async ({params}:{ params:{depNom : string}}) => {
   const DepNom = await params.depNom;
+    const user= await auth();
   const search = Categories.filter((cat) => cat.link === DepNom);
   const Items = await getItemByCategory(search[0].dep);
+  if(!user)  return(
+        <section className='w-full'>
+            <Loader/>
+        </section>
+      )
   return (
     <section>
     <DepItems items={Items} />
@@ -14,3 +24,5 @@ const Page = async ({params}:{ params:{depNom : string}}) => {
 }
 
 export default Page
+
+const Page = async () => {
