801ad61324c7286987fedec5243924a3631e07a8
yassinkr <kerraiyassin@gmail.com>
2025-01-11 12:04:14 +0100

    final

--- src/app/(root)/orders/page.tsx
+++ src/app/(root)/orders/page.tsx
@@ -16,7 +16,7 @@ const Page = async () => {
       <h1 className='text-xl font-bold text-black-1 dark:text-white-1'>Commandes En attente</h1>
       {orders.length > 0 ? <div className='Cours_grid'>
         {orders.map((cours,index) => (
-          <OrderCard route={"archive"} idSimple={cours.id}   key={index} id={cours.identifier} Prix={cours.Prix}  CreatedAt={cours.createdAt}/>))}
+          <OrderCard route={"orders"} idSimple={cours.id}   key={index} id={cours.identifier} Prix={cours.Prix}  CreatedAt={cours.createdAt}/>))}
         </div>:<EmptyState title={'No Results found'}/>}
       </div>
     </section>
