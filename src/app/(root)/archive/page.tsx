9d2a1a9cb138693219da26271c17dada57a5fa53
LectioEditions <LectioEditions@outlook.com>
2025-01-17 14:10:34 +0100

    Update page.tsx

--- src/app/(root)/archive/page.tsx
+++ src/app/(root)/archive/page.tsx
@@ -8,13 +8,19 @@ import { auth } from '@clerk/nextjs/server';
 const Page = async () => {
   const user= await auth();
   const orders= await getAllOrders();
+    const sortedOrders = orders.sort((a, b) => {
+    // Ensure that both items have the createdAt field, and compare
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
+    return dateB.getTime() - dateA.getTime(); // descending order
+  });
   if(!orders || !user) return <Loader size={50} className='mx-auto'/>;
   return (
     <section className=" w-full min-h-screen  py-10">
       <div className='flex flex-col gap-9'>
       <h1 className='text-xl font-bold text-black-1 dark:text-white-1'>Commandes</h1>
-       {orders.length > 0 ? <div className='Cours_grid'>
-        {orders.map((cours,index) => (
+       {sortedOrders.length > 0 ? <div className='Cours_grid'>
+        {sortedOrders.map((cours,index) => (
           <OrderCard route={"archive"} idSimple={cours.id}   key={index} id={cours.identifier} Prix={cours.Prix}  CreatedAt={cours.createdAt}/>))}
         </div>:<EmptyState title={'No Results found'}/>}
        
